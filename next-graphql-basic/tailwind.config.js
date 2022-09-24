1d760afb5a800094def83a246b7344df726087a1
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-09-24 11:44:25 +0900

    feat: Add layout component

--- next-graphql-basic/tailwind.config.js
+++ next-graphql-basic/tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false,
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
